709c69a726ed67cca07f3a87e5af054a83b54ce0
salazarka <31709585+salazarka@users.noreply.github.com>
2018-08-30 23:06:21 -0600

    Logica de juego
    Logica de juego con matriz y cambio de jugador

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -8,11 +8,25 @@ export class DashboardComponent implements OnInit {
   tam: number = 4;
   cant: number = 2; // guarda la cantidad de fichas de gane
   manejoFichas: number = 1; // para el cambio de color de fichas
-
+  turno : number = 1 ;//1:J1 - 2:J2
+  matrix:number[][]=new Array();//REPLICAR DE LA MATRIZ DE JUEGO
+  
   // @ViewChild('canvasRef') canvasRef: ElementRef; referencia elementos html
   constructor() { }
   ngOnInit() {
   }
+
+   //FUNCION PARA REPLICAR EL JUEGO EN UNA MATRIZ
+   crearMatriz(){ 
+    for (let a = 0; a < this.tam; a++) {
+      let array = new Array;
+      for(let s = 0; s < this.tam; s++){
+        array[s] = 0;//CREA UN ARRAY PARA CADA FILA
+      }
+      this.matrix[a] = array//UNE CADA ARRAY,FORMA MATRIZ
+    }
+  }
+
   // -----------------------------------------------------------------------------------------------------
   // FUNCIÓN QUE PINTA UN CUADRO CUANDO LEE UN CLICK EN EL CANVAS
   // -----------------------------------------------------------------------------------------------------
@@ -31,7 +45,25 @@ export class DashboardComponent implements OnInit {
       for (let x = 0; x <= this.tam; x++) {// voy a suponer que el tablero es de tam 4
         for (let y = 0; y <= this.tam; y++) {
           if ( (i >= x * 90 && i <= x * 90 + 90) && (j >= y * 90 && j <= y * 90 + 90) ) {
-            if ( this.manejoFichas % 2 === 0) {
+            if (this.matrix[y][x] == 0) { //VERIFICA QUE LA CASILLA NO TIENE FICHA
+              if (this.turno == 1) {//JUGADOR 1
+                this.matrix[y][x] = 1;//MARCA LA EN LA MATRIZ CON LA FICHA JUGADOR 1
+                this.turno = 2;//CAMBIO DE TURNO
+                ctx.fillStyle = 'rgb(160, 140, 160)'; // DEFINE EL COLOR DE LA FIGUTA
+                ctx.fillRect(x * 90, y * 90, 90, 90);
+              }
+              else if (this.turno == 2) {//JUGADOR 2
+                this.matrix[y][x] = 2//MARCA LA EN LA MATRIZ CON LA FICHA JUGADOR 2
+                this.turno = 1//CAMBIO DE TURNO
+                ctx.fillStyle = 'rgb(190, 100, 80)'; // DEFINE EL COLOR DE LA FIGUTA
+                ctx.fillRect(x * 90, y * 90, 90, 90);
+              }
+              //console.log(this.matrix)
+            }
+            else { 
+              alert("Jugada no válida")
+            }
+            /*if ( this.manejoFichas % 2 === 0) {
               ctx.fillStyle = 'rgb(160, 140, 160)'; // DEFINE EL COLOR DE LA FIGUTA
               ctx.fillRect(x * 90, y * 90, 90, 90); // DEFINE LA POSICIÒN A PINTAR´
               this.manejoFichas = this.manejoFichas + 1;
@@ -39,14 +71,11 @@ export class DashboardComponent implements OnInit {
               ctx.fillStyle = 'rgb(190, 100, 80)'; // DEFINE EL COLOR DE LA FIGUTA
               ctx.fillRect(x * 90, y * 90, 90, 90); // DEFINE LA POSICIÒN A PINTAR´
               this.manejoFichas = this.manejoFichas + 1;
-            }
+            }*/
           }
         }
       }
     }
-
-
-
   }
   // -----------------------------------------------------------------------------------------------------
   // FUNCIÓN QUE SE ENCARGA DE CREAR EL TABLERO DE JUEGO
@@ -56,6 +85,7 @@ export class DashboardComponent implements OnInit {
     canvas.width = tamano * 90;  // VARIABLES QUE ACTUALIZAN LOS VALORES DEL CANVAS DE ACUERDO AL TAMAÑO DEL TABLERO
     canvas.height = tamano * 90;
     console.log(tamano);
+    this.crearMatriz()
     if (canvas.getContext) {
       const ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height); // LIMPIA EL CANVAS
